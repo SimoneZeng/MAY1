5c0fd65fb1dd779e78cef7db8c863052e20c7877
SimoneZeng <1481570908@qq.com>
2022-09-07 11:27:49 +0800

    simplify the logic in get_all

--- dqn_train.py
+++ dqn_train.py
@@ -11,26 +11,26 @@ sumolib:1.14.0
 @author: Skye
 """
 
+
 import numpy as np
 import argparse
 from copy import deepcopy
 import matplotlib.pyplot as plt
 import torch
 import random
-#from traci_generation import generate_routefile
 from dqn_model import DQN
-#from queue import Queue
 import os, sys
 import time
 import traci
 from sumolib import checkBinary
 import pandas as pd
+import random
 
 
 # 引入地址 
 sumo_path = os.environ['SUMO_HOME'] # "D:\\sumo\\sumo1.13.0"
-project_path = "E:\condaProject\sumo_test01\sumo"
-cfg_path = "E:\condaProject\sumo_test01\sumo\one_way.sumocfg"
+project_path = "E:\condaProject\sumo_intersection\sumo_test01\sumo"
+cfg_path = "E:\condaProject\sumo_intersection\sumo_test01\sumo\one_way.sumocfg"
 sys.path.append(sumo_path)
 sys.path.append(sumo_path + "tools")
 sys.path.append(sumo_path + "/tools/xml")
@@ -54,19 +54,16 @@ else:
 #    sumoBinary = sumo_path + "/bin/sumo"
 
 sumoCmd = [sumoBinary, "-c", cfg_path]
-#sumoCmd = [sumoBinary, "-c", cfg_path, '--lanechange.duration','0.5','--route-steps','0','--seed','42']
 
 # 记录车辆信息
 map_ve = {}
 # 包括前一时刻的后方第一辆车和当前时刻的后方第一辆车
 back_id = [' ', ' ']
-
 auto_vehicle_a = 0
 # 统计总的训练次数
 step = 0
 # 存储奖励值的数组，分epo存的
-all_reward = []
-cols = ['epo', 'train_step', 'position_y', 'lane', 's', 'a', 'r', 's_']
+cols = ['epo', 'train_step', 'position_y', 'target_lane', 'lane', 's', 'a', 'r', 's_']
 df_record = pd.DataFrame(columns = cols)
 
 
@@ -77,26 +74,25 @@ def get_all(control_vehicle, select_dis):
     :param select_dis: 探测车辆的范围，[-select_dis, select_dis]
     :return:
     """
-
-    # 将自动驾驶车要用到的周围车的信息保存在map_ve字典里，key是周围车辆的ID，每个周围车辆的数据为
-    # [该车相对于自动驾驶车的相对纵向距离，相对横向距离，相对速度]
-    global map_ve
-    vehicle_list = traci.vehicle.getIDList()
-    for vehicle in vehicle_list:
-        if vehicle not in map_ve.keys():
-            map_ve[vehicle]=[traci.vehicle.getPosition(vehicle)[0]-traci.vehicle.getPosition(control_vehicle)[0],
-                             traci.vehicle.getPosition(vehicle)[1]-traci.vehicle.getPosition(control_vehicle)[1],
-                             traci.vehicle.getSpeed(vehicle)-traci.vehicle.getSpeed(control_vehicle)]
-        else:
-            map_ve[vehicle]=[traci.vehicle.getPosition(vehicle)[0]-traci.vehicle.getPosition(control_vehicle)[0],
-                             traci.vehicle.getPosition(vehicle)[1]-traci.vehicle.getPosition(control_vehicle)[1],
-                             traci.vehicle.getSpeed(vehicle)-traci.vehicle.getSpeed(control_vehicle)]
-
     # 获取自动驾驶车的Y坐标和X坐标和纵向速度和车道
     y_pos = traci.vehicle.getPosition(control_vehicle)[0]
     x_pos = traci.vehicle.getPosition(control_vehicle)[1]
     y_speed = traci.vehicle.getSpeed(control_vehicle)
-    lane = traci.vehicle.getLaneID(control_vehicle)
+    ego_lane = traci.vehicle.getLaneID(control_vehicle)
+    
+    # 将自动驾驶车要用到的周围车的信息保存在map_ve字典里，key是周围车辆的ID，每个周围车辆的数据为
+    # [该车相对于自动驾驶车的相对纵向距离，相对横向距离，相对速度]
+    global map_ve
+    vehicle_list = traci.vehicle.getIDList()
+    for v in vehicle_list:
+        if v not in map_ve.keys():
+            map_ve[v]=[traci.vehicle.getPosition(v)[0] - y_pos,
+                             traci.vehicle.getPosition(v)[1] - x_pos,
+                             traci.vehicle.getSpeed(v) - y_speed]
+        else:
+            map_ve[v]=[traci.vehicle.getPosition(v)[0] - y_pos,
+                             traci.vehicle.getPosition(v)[1] - x_pos,
+                             traci.vehicle.getSpeed(v) - y_speed]
 
     # 总的需要收集的车辆信息的数组
     Id_list = []
@@ -109,27 +105,27 @@ def get_all(control_vehicle, select_dis):
     downleft = []
 
     # 将六个方向的车收集到分别的数组里面去
-    for select_vehicle in vehicle_list:
-        if select_vehicle == control_vehicle:
+    for v in vehicle_list:
+        if v == control_vehicle:
             continue
         # 正前方
-        if y_pos + select_dis > traci.vehicle.getPosition(select_vehicle)[0] > y_pos and np.abs(traci.vehicle.getPosition(select_vehicle)[1]-x_pos) < 1:
-            up.append([traci.vehicle.getPosition(select_vehicle)[0] - y_pos, select_vehicle])
+        if y_pos + select_dis > traci.vehicle.getPosition(v)[0] > y_pos and np.abs(traci.vehicle.getPosition(v)[1]-x_pos) < 1:
+            up.append([traci.vehicle.getPosition(v)[0] - y_pos, v])
         # 右前方
-        if y_pos + select_dis > traci.vehicle.getPosition(select_vehicle)[0] > y_pos and 2 < x_pos - traci.vehicle.getPosition(select_vehicle)[1] < 4:
-            upright.append([traci.vehicle.getPosition(select_vehicle)[0] - y_pos, select_vehicle])
+        if y_pos + select_dis > traci.vehicle.getPosition(v)[0] > y_pos and 2 < x_pos - traci.vehicle.getPosition(v)[1] < 4:
+            upright.append([traci.vehicle.getPosition(v)[0] - y_pos, v])
         # 左前方
-        if y_pos + select_dis > traci.vehicle.getPosition(select_vehicle)[0] > y_pos and 2 < traci.vehicle.getPosition(select_vehicle)[1]-x_pos < 4:
-            upleft.append([traci.vehicle.getPosition(select_vehicle)[0] - y_pos, select_vehicle])
+        if y_pos + select_dis > traci.vehicle.getPosition(v)[0] > y_pos and 2 < traci.vehicle.getPosition(v)[1]-x_pos < 4:
+            upleft.append([traci.vehicle.getPosition(v)[0] - y_pos, v])
         # 正后方，防止select_dis范围内没有后车，所有不设范围
-        if traci.vehicle.getPosition(select_vehicle)[0] < y_pos and np.abs(traci.vehicle.getPosition(select_vehicle)[1]-x_pos) < 1:
-            down.append([traci.vehicle.getPosition(select_vehicle)[0] - y_pos, select_vehicle])
+        if traci.vehicle.getPosition(v)[0] < y_pos and np.abs(traci.vehicle.getPosition(v)[1]-x_pos) < 1:
+            down.append([traci.vehicle.getPosition(v)[0] - y_pos, v])
         # 右后方
-        if y_pos - select_dis < traci.vehicle.getPosition(select_vehicle)[0] < y_pos and 2 < x_pos - traci.vehicle.getPosition(select_vehicle)[1] < 4:
-            downright.append([traci.vehicle.getPosition(select_vehicle)[0] - y_pos, select_vehicle])
+        if y_pos - select_dis < traci.vehicle.getPosition(v)[0] < y_pos and 2 < x_pos - traci.vehicle.getPosition(v)[1] < 4:
+            downright.append([traci.vehicle.getPosition(v)[0] - y_pos, v])
         # 左后方
-        if y_pos - select_dis < traci.vehicle.getPosition(select_vehicle)[0] < y_pos and 2 < traci.vehicle.getPosition(select_vehicle)[1]-x_pos < 4:
-            downleft.append([traci.vehicle.getPosition(select_vehicle)[0] - y_pos, select_vehicle])
+        if y_pos - select_dis < traci.vehicle.getPosition(v)[0] < y_pos and 2 < traci.vehicle.getPosition(v)[1]-x_pos < 4:
+            downleft.append([traci.vehicle.getPosition(v)[0] - y_pos, v])
 
     # 排序操作是为了找到那个方向离自动驾驶车最近的那辆车，（每个方向只选了最近的那辆）
     up = sorted(up, key=lambda x: x[0])
@@ -145,7 +141,7 @@ def get_all(control_vehicle, select_dis):
     if len(upright) >= 1:
         Id_list.append(map_ve[upright[0][-1]])
     else:
-        if '0' in lane:
+        if '0' in ego_lane:
             Id_list.append([0, -3.2, 0])
         else:
             Id_list.append([200, -3.2, 0])
@@ -154,7 +150,7 @@ def get_all(control_vehicle, select_dis):
     if len(upleft) >= 1:
         Id_list.append(map_ve[upleft[0][-1]])
     else:
-        if '2' in lane:
+        if '2' in ego_lane:
             Id_list.append([0, 3.2, 0])
         else:
             Id_list.append([200, 3.2, 0])
@@ -171,7 +167,7 @@ def get_all(control_vehicle, select_dis):
     if len(downright) >= 1:
         Id_list.append(map_ve[downright[-1][-1]])
     else:
-        if '0' in lane:
+        if '0' in ego_lane:
             Id_list.append([0, -3.2, 0])
         else:
             Id_list.append([-200, -3.2, 0])
@@ -180,7 +176,7 @@ def get_all(control_vehicle, select_dis):
     if len(downleft) >= 1:
         Id_list.append(map_ve[downleft[-1][-1]])
     else:
-        if '2' in lane:
+        if '2' in ego_lane:
             Id_list.append([0, 3.2, 0])
         else:
             Id_list.append([-200, 3.2, 0])
@@ -191,7 +187,6 @@ def get_all(control_vehicle, select_dis):
     back_id[1] = back_v
 
     # 得到自动驾驶车自己所在的车道
-    ego_lane=traci.vehicle.getLaneID(control_vehicle)
     if '0' in ego_lane:
         ego_l=-1
     elif '1' in ego_lane:
@@ -212,21 +207,33 @@ def get_all(control_vehicle, select_dis):
         Id_list[i][2] = Id_list[i][2]/25
 
     # 为了得到与前车的相对纵向距离和相对纵向速度
+    # 前方车辆可能不在200m内，不能直接用up中的数据
     relspace = 100000
     relspeed = 0
-    for select_vehicle in vehicle_list:
-        if select_vehicle == control_vehicle:
+    for v in vehicle_list:
+        if v == control_vehicle:
             continue
-        if traci.vehicle.getPosition(select_vehicle)[0] - y_pos > 0 and traci.vehicle.getLaneID(select_vehicle) == traci.vehicle.getLaneID(control_vehicle):
-            if traci.vehicle.getPosition(select_vehicle)[0] - y_pos < relspace:
-                relspace = traci.vehicle.getPosition(select_vehicle)[0] - y_pos
-                relspeed = traci.vehicle.getSpeed(select_vehicle) - y_speed
+        if traci.vehicle.getPosition(v)[0] - y_pos > 0 and traci.vehicle.getLaneID(v) == ego_lane:
+            if traci.vehicle.getPosition(v)[0] - y_pos < relspace:
+                relspace = traci.vehicle.getPosition(v)[0] - y_pos
+                relspeed = traci.vehicle.getSpeed(v) - y_speed
+    
+    # 前后200m范围内容的车道级流量
+    flow_left = len(upleft) + len(downleft)
+    flow_middle = len(up) + len(down)
+    flow_right = len(upright) + len(downright)
+    flow = {'flow_left': flow_left, 'flow_middle': flow_middle, 'flow_right': flow_right}
+    
+    rel_up = {'relspace': relspace, 'relspeed':relspeed}
+    
     # 周围车信息，相对距离，相对速度
-    return Id_list, relspace, relspeed
+    return Id_list, rel_up, flow
 
 
-def train(agent, all_vehicle, control_vehicle, episode, train_step, index):
+def train(agent, control_vehicle, episode, train_step, target_lane):
+    all_vehicle, rel_up, _ = get_all(control_vehicle, 200)
     action_int = agent.choose_action(np.array(all_vehicle)) # 返回第action_int个动作 0-8
+    
     # 012位指加速 345指不变速 678指减速；然后036指左变道，147指不变道，258指右变道
     if action_int in [0, 1, 2]:
         acc= 3
@@ -281,7 +288,7 @@ def train(agent, all_vehicle, control_vehicle, episode, train_step, index):
         return collision
     
     # 获取动作执行后的状态
-    new_all_vehicle, rel_space, rel_speed = get_all(control_vehicle, 200)
+    new_all_vehicle, new_rel_up, _ = get_all(control_vehicle, 200)
     cur_ego_info_dict = {"speed": traci.vehicle.getSpeed(control_vehicle), 
                      "acc":traci.vehicle.getAcceleration(control_vehicle), 
                      "LaneID": traci.vehicle.getLaneID(control_vehicle), 
@@ -289,30 +296,30 @@ def train(agent, all_vehicle, control_vehicle, episode, train_step, index):
       
     # 避免分母为0
     e = 0.000001
-    if 0 <= rel_speed < e:
-        rel_speed = e
-    if -e < rel_speed < 0:
-        rel_speed = -e
+    if 0 <= new_rel_up['relspeed'] < e:
+        new_rel_up['relspeed'] = e
+    if -e < new_rel_up['relspeed'] < 0:
+        new_rel_up['relspeed'] = -e
     
     # 计算reward
-    y_ttc=-rel_space/rel_speed
+    y_ttc=-new_rel_up['relspace']/new_rel_up['relspeed'] # time to collision
     r_efficiency = cur_ego_info_dict['speed']/25*0.8
     
-    if 0 < y_ttc < 4:
+    if 0 < y_ttc < 4: 
         r_safe = np.log(y_ttc/4)
     else:
         r_safe = 0
     
     auto_vehicle_a = cur_ego_info_dict['acc']
     r_comfort = ((np.abs(pre_ego_info_dict['acc']-cur_ego_info_dict['acc'])/0.1) ** 2) / 3600
+    r_target_lane = -0.5 * abs(int(cur_ego_info_dict['LaneID'][-1]) - target_lane) # 与目标车道的差距绝对值，*0.5，分别为 0 -0.5 -1
     
-    cur_reward = r_safe + r_efficiency - r_comfort
-    global all_reward
-    all_reward.append([cur_reward, episode, index])
+    cur_reward = r_safe + r_efficiency - r_comfort + r_target_lane
+    
     global df_record
     df_record = df_record.append(pd.DataFrame([[episode, train_step, pre_ego_info_dict['position'][0], 
-                                    pre_ego_info_dict['LaneID'], all_vehicle, action_int, 
-                                    cur_reward, new_all_vehicle]], columns = cols))
+                                                target_lane, pre_ego_info_dict['LaneID'], all_vehicle, 
+                                                action_int, cur_reward, new_all_vehicle]], columns = cols))
     
     # agent存储
     agent.store_transition(all_vehicle, action_int, cur_reward, new_all_vehicle)
@@ -331,12 +338,15 @@ def main_train():
     for epo in range(1):
         traci.start(sumoCmd)
         ego_index = 20 + epo % 100   # 选取中间车道第index辆出发的车为我们的自动驾驶车
-        ego_index_str = '1_'+str(ego_index) # 自动驾驶车辆的id为'1_$index$', 如index为20,id='1_20'
-        control_vehicle = '' # 自动驾驶车辆的id
-        ego_show = False # 自动驾驶车辆是否出现过
+        ego_index_str = '1_'+str(ego_index) # ego的id为'1_$index$', 如index为20,id='1_20'
+        control_vehicle = '' # ego车辆的id
+        ego_show = False # ego车辆是否出现过
+        target_lane = random.randint(0, 2) # ego的变道目标车道，从0 1 2中取
         global auto_vehicle_a
         auto_vehicle_a = 0
         train_step = 0
+        global df_record
+        df_record = pd.DataFrame(columns = cols)
         
         while traci.simulation.getMinExpectedNumber() > 0:
             # 1. 得到道路上所有的车辆ID
@@ -371,7 +381,7 @@ def main_train():
     
             # 4 在RL控制路段中收集自动驾驶车周围车辆的信息，并设置周围车辆
             # 4.1 获取周围车辆信息
-            all_vehicle, re_sp, relspeed = get_all(control_vehicle, 200) # 前后200m距离
+#            all_vehicle, rel_up, _ = get_all(control_vehicle, 200) # 前后200m距离
     
             # 4.2 将所有后方车辆设置为不变道
             for vehicle in vehicle_list:
@@ -383,7 +393,7 @@ def main_train():
             traci.vehicle.setLaneChangeMode(control_vehicle, 0b000000000000)
             
             # 5 模型训练
-            collision = train(agent, all_vehicle, control_vehicle, epo, train_step, ego_index)
+            collision = train(agent, control_vehicle, epo, train_step, target_lane)
             if collision:
                 break
             train_step = train_step + 1
@@ -392,12 +402,16 @@ def main_train():
 #        np.save("reward/"+str(epo)+"r.npy", np.array(all_reward))
         traci.close(wait=True)
         # 保存
+        df_record.to_csv(f"result_record/df_record_epo{epo}.csv", index = False)
 #        agent.save_net()
 
 
 
 if __name__ == '__main__':
     main_train() 
-    df_record.to_csv("df_record.csv", index = False)
+#    df_record.to_csv("df_record.csv", index = False)
+#    pass
+
+
 
 
